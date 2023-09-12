d91d7d5ff148d2aff258d371768452c294985d1a
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-09-12 03:20:09 -0300

    UI improvment

--- docs/script.js
+++ docs/script.js
@@ -5,26 +5,19 @@ const fetchcep = async () => {
     const url = 'https://cep.awesomeapi.com.br/json/'
     const inputcep = document.getElementById('cepinput').value
 
-    const { cep, address, state, district, city, lat: fetchedLat, lng: fetchedLng } = await fetch(url + inputcep)
+    const { cep, lat, lng, address, state, district, city} = await fetch(url + inputcep)
         .then(adress => adress.json())
 
-    if (typeof fetchedLat !== 'undefined' && typeof fetchedLng !== 'undefined') {
-        lat = fetchedLat;
-        lng = fetchedLng;
+    if (map) {
+    map.setView([lat, lng], 13);
+    marker.setLatLng([lat, lng]);
     }
-
     console.log(cep);
 
     document.getElementById("printCity").innerHTML = `City: ${city} ${state}`
-    document.getElementById("printState").innerHTML = ` ${state}`
     document.getElementById("printAddress").innerHTML = `Address: ${address}`
     document.getElementById("printDistrict").innerHTML = `District: ${district}`
 
-    if (map) {
-        map.setView([lat, lng], 13);
-        marker.setLatLng([lat, lng]);
-    }
-    
 }
 
 var map = L.map('map').setView([lat, lng], 13);
