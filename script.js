7efe025fb10b57189a05a04bb5e297f1ab372252
Bruno Merico Mozer <brunomozer05@gmail.com>
2023-09-18 04:15:22 -0300

    Optimized CSS and formatted CSS/JS

--- script.js
+++ script.js
@@ -2,30 +2,28 @@ let lat = -27.5969;
 let lng = -48.5495;
 
 const fetchcep = async () => {
-    const url = 'https://cep.awesomeapi.com.br/json/'
-    const inputcep = document.getElementById('cepinput').value
+  const url = "https://cep.awesomeapi.com.br/json/";
+  const inputcep = document.getElementById("cepinput").value;
 
-    const { cep, lat, lng, address, state, district, city} = await fetch(url + inputcep)
-        .then(adress => adress.json())
+  const { cep, lat, lng, address, state, district, city } = await fetch(
+    url + inputcep
+  ).then((adress) => adress.json());
 
-    if (map) {
+  if (map) {
     map.setView([lat, lng], 13);
     marker.setLatLng([lat, lng]);
-    }
-    console.log(cep);
+  }
+  console.log(cep);
 
-    document.getElementById("printCity").innerHTML = `City: ${city} ${state}`
-    document.getElementById("printAddress").innerHTML = `Address: ${address}`
-    document.getElementById("printDistrict").innerHTML = `District: ${district}`
+  document.getElementById("printCity").innerHTML = `City: ${city} ${state}`;
+  document.getElementById("printAddress").innerHTML = `Address: ${address}`;
+  document.getElementById("printDistrict").innerHTML = `District: ${district}`;
+};
 
-}
+var map = L.map("map").setView([lat, lng], 13);
 
-var map = L.map('map').setView([lat, lng], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-}).addTo(map);
-
-var marker = L.marker([lat, lng]).addTo(map)
-    .openPopup(); 
+var marker = L.marker([lat, lng]).addTo(map).openPopup();
 
 fetchcep();
